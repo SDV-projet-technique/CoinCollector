0b795b2784573ca7d8ec885b37f342bf2e4267bf
Dylan GIL AMARO <dylan.flame.libra@gmail.com>
2024-05-23 11:44:49 +0200

    :lipstick: display coins in Card

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,4 @@
 import { Stack } from "expo-router";
-import * as SQLite from "expo-sqlite";
 import { SQLiteProvider } from "expo-sqlite";
 import {
   MD3LightTheme as DefaultTheme,
@@ -9,45 +8,45 @@ import {
 const theme = {
   ...DefaultTheme,
   colors: {
-    primary: "rgb(248, 189, 42)",
-    onPrimary: "rgb(64, 45, 0)",
-    primaryContainer: "rgb(92, 67, 0)",
-    onPrimaryContainer: "rgb(255, 223, 160)",
-    secondary: "rgb(216, 196, 160)",
-    onSecondary: "rgb(59, 47, 21)",
-    secondaryContainer: "rgb(83, 69, 42)",
-    onSecondaryContainer: "rgb(245, 224, 187)",
-    tertiary: "rgb(177, 207, 169)",
-    onTertiary: "rgb(29, 54, 27)",
-    tertiaryContainer: "rgb(51, 77, 48)",
-    onTertiaryContainer: "rgb(204, 235, 196)",
-    error: "rgb(255, 180, 171)",
-    onError: "rgb(105, 0, 5)",
-    errorContainer: "rgb(147, 0, 10)",
-    onErrorContainer: "rgb(255, 180, 171)",
-    background: "rgb(30, 27, 22)",
-    onBackground: "rgb(233, 225, 216)",
-    surface: "rgb(30, 27, 22)",
-    onSurface: "rgb(233, 225, 216)",
-    surfaceVariant: "rgb(77, 70, 57)",
-    onSurfaceVariant: "rgb(208, 197, 180)",
-    outline: "rgb(153, 143, 128)",
-    outlineVariant: "rgb(77, 70, 57)",
+    primary: "rgb(121, 89, 0)",
+    onPrimary: "rgb(255, 255, 255)",
+    primaryContainer: "rgb(255, 223, 160)",
+    onPrimaryContainer: "rgb(38, 26, 0)",
+    secondary: "rgb(107, 92, 63)",
+    onSecondary: "rgb(255, 255, 255)",
+    secondaryContainer: "rgb(245, 224, 187)",
+    onSecondaryContainer: "rgb(36, 26, 4)",
+    tertiary: "rgb(74, 101, 70)",
+    onTertiary: "rgb(255, 255, 255)",
+    tertiaryContainer: "rgb(204, 235, 196)",
+    onTertiaryContainer: "rgb(8, 32, 8)",
+    error: "rgb(186, 26, 26)",
+    onError: "rgb(255, 255, 255)",
+    errorContainer: "rgb(255, 218, 214)",
+    onErrorContainer: "rgb(65, 0, 2)",
+    background: "rgb(255, 251, 255)",
+    onBackground: "rgb(30, 27, 22)",
+    surface: "rgb(255, 251, 255)",
+    onSurface: "rgb(30, 27, 22)",
+    surfaceVariant: "rgb(237, 225, 207)",
+    onSurfaceVariant: "rgb(77, 70, 57)",
+    outline: "rgb(127, 118, 103)",
+    outlineVariant: "rgb(208, 197, 180)",
     shadow: "rgb(0, 0, 0)",
     scrim: "rgb(0, 0, 0)",
-    inverseSurface: "rgb(233, 225, 216)",
-    inverseOnSurface: "rgb(52, 48, 42)",
-    inversePrimary: "rgb(121, 89, 0)",
+    inverseSurface: "rgb(52, 48, 42)",
+    inverseOnSurface: "rgb(248, 239, 231)",
+    inversePrimary: "rgb(248, 189, 42)",
     elevation: {
       level0: "transparent",
-      level1: "rgb(41, 35, 23)",
-      level2: "rgb(47, 40, 24)",
-      level3: "rgb(54, 45, 24)",
-      level4: "rgb(56, 46, 24)",
-      level5: "rgb(61, 50, 25)",
+      level1: "rgb(248, 243, 242)",
+      level2: "rgb(244, 238, 235)",
+      level3: "rgb(240, 233, 227)",
+      level4: "rgb(239, 232, 224)",
+      level5: "rgb(236, 228, 219)",
     },
-    surfaceDisabled: "rgba(233, 225, 216, 0.12)",
-    onSurfaceDisabled: "rgba(233, 225, 216, 0.38)",
+    surfaceDisabled: "rgba(30, 27, 22, 0.12)",
+    onSurfaceDisabled: "rgba(30, 27, 22, 0.38)",
     backdrop: "rgba(54, 48, 36, 0.4)",
   },
 };
@@ -56,61 +55,10 @@ export default function RootLayout() {
   return (
     <PaperProvider theme={theme}>
       <SQLiteProvider databaseName="app.db">
-        {/* <SQLiteProvider databaseName="app.db" onInit={migrateDbIfNeeded}> */}
         <Stack>
           <Stack.Screen name="index" options={{ headerShown: false }} />
         </Stack>
       </SQLiteProvider>
     </PaperProvider>
   );
-}
-
-async function migrateDbIfNeeded(db: SQLite.SQLiteDatabase) {
-  const DATABASE_VERSION = 1;
-  let { user_version: currentDbVersion } = (await db.getFirstAsync<{
-    user_version: number;
-  }>("PRAGMA user_version")) || { user_version: 0 };
-  console.log("currentDbVersion", currentDbVersion);
-  console.log("DATABASE_VERSION", DATABASE_VERSION);
-  // await db.execAsync(`
-  // DROP TABLE IF EXISTS coins;
-  // `);
-  // console.log("droped");
-  if (currentDbVersion >= DATABASE_VERSION) {
-    return;
-  }
-  console.log("currentDbVersion", currentDbVersion);
-  currentDbVersion = 0;
-  console.log("currentDbVersion", currentDbVersion);
-  if (currentDbVersion === 0) {
-    await db.execAsync(`
-  PRAGMA journal_mode = 'wal';
-  CREATE TABLE coins ( 
-      id INTEGER PRIMARY KEY AUTOINCREMENT,
-      annee INTEGER NOT NULL,
-      rarete TEXT NOT NULL,
-      quantite INTEGER NOT NULL,
-      valeur TEXT);
-  `);
-    await db.runAsync(
-      "INSERT INTO coins (annee, rarete, quantite, valeur)",
-      2021,
-      "Rare",
-      10,
-      "[1.5, 2.0, 2.5]"
-    );
-    await db.runAsync(
-      "INSERT INTO coins (annee, rarete, quantite, valeur)",
-      2022,
-      "Common",
-      20,
-      "[1.0, 1.5]"
-    );
-    currentDbVersion = 1;
-    console.log("finished");
-  }
-  // if (currentDbVersion === 1) {
-  //   Add more migrations
-  // }
-  await db.execAsync(`PRAGMA user_version = ${DATABASE_VERSION}`);
 }
--- app/index.tsx
+++ app/index.tsx
@@ -1,14 +1,22 @@
 import * as SQLite from "expo-sqlite";
 import { useEffect, useState } from "react";
-import { AppRegistry, Text, View } from "react-native";
-import { Button } from "react-native-paper";
+import {
+  AppRegistry,
+  SafeAreaView,
+  ScrollView,
+  StyleSheet,
+  Text,
+} from "react-native";
+import { Card } from "react-native-paper";
 
 interface Coin {
   id: number;
+  name: string;
   annee: number;
   rarete: string;
   quantite: number;
   valeur: string;
+  image: string;
 }
 
 export default function Index() {
@@ -18,14 +26,18 @@ export default function Index() {
   useEffect(() => {
     const initializeDatabase = async () => {
       await db.withTransactionAsync(async () => {
+        // await db.execAsync("DROP TABLE IF EXISTS coins");
         await db.execAsync(`
           PRAGMA journal_mode = 'wal';
           CREATE TABLE IF NOT EXISTS coins (
             id INTEGER PRIMARY KEY AUTOINCREMENT,
+            name TEXT NOT NULL,
             annee INTEGER NOT NULL,
             rarete TEXT NOT NULL,
             quantite INTEGER NOT NULL,
-            valeur TEXT);
+            valeurs TEXT,
+            image TEXT
+          );
         `);
 
         const result = await db.getAllAsync<{ count: number }>(
@@ -33,12 +45,26 @@ export default function Index() {
         );
         if (result.length > 0 && result[0].count === 0) {
           await db.runAsync(
-            "INSERT INTO coins (annee, rarete, quantite, valeur) VALUES (?, ?, ?, ?)",
-            [2021, "Rare", 10, "[1.5, 2.0, 2.5]"]
+            "INSERT INTO coins (name, annee, rarete, quantite, valeur, image) VALUES (?, ?, ?, ?, ?, ?)",
+            [
+              "5 FRANCS OR - NAPOLÉON III TÊTE NUE",
+              2021,
+              "Rare",
+              10,
+              "[1.5, 2.0, 2.5]",
+              "https://www.laposte.fr/ecom/occ/ecommerce/medias/sys_master/images/ha3/hd7/12607409586206/logo-euros-commemoratives/logo-euros-commemoratives.png",
+            ]
           );
           await db.runAsync(
-            "INSERT INTO coins (annee, rarete, quantite, valeur) VALUES (?, ?, ?, ?)",
-            [2022, "Common", 20, "[1.0, 1.5]"]
+            "INSERT INTO coins (name, annee, rarete, quantite, valeur, image) VALUES (?, ?, ?, ?, ?, ?)",
+            [
+              "10 FRANCS OR - NAPOLÉON III TÊTE NUE",
+              2022,
+              "Common",
+              20,
+              "[1.0, 1.5]",
+              "https://goldunion.fr/cdn/shop/files/250-napo-face-off.png?v=1703878391&width=1080",
+            ]
           );
         }
       });
@@ -51,37 +77,39 @@ export default function Index() {
   }, []);
 
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Coins</Text>
-      {coins.map((coin, index) => (
-        <View key={index}>
-          <Text>{coin.annee}</Text>
-          <Text>{coin.rarete}</Text>
-          <Text>{coin.quantite}</Text>
-          <Text>{coin.valeur}</Text>
-        </View>
-      ))}
-      <Text>Edit app/index.tsx to edit this screen.</Text>
-      <Button
-        icon="camera"
-        mode="contained"
-        onPress={() => {
-          console.log("Button pressed");
-          db.getAllAsync<Coin>("SELECT * FROM coins").then((coins) => {
-            console.log("coins", coins);
-          });
-        }}
-      >
-        Press me
-      </Button>
-    </View>
+    <SafeAreaView style={styles.container}>
+      <ScrollView contentContainerStyle={styles.scrollView}>
+        <Text>Coins:</Text>
+        {coins.map((coin, index) => (
+          <Card key={index} style={{ width: "60%", margin: 10 }}>
+            <Card.Cover source={{ uri: coin.image }} />
+            <Card.Title title={coin.name} key={index} />
+            <Card.Content>
+              <Text>Year: {coin.annee}</Text>
+              <Text>Rarity: {coin.rarete}</Text>
+              <Text>Quantity: {coin.quantite}</Text>
+              <Text>Value: {JSON.parse(coin.valeur).pop()}€</Text>
+            </Card.Content>
+          </Card>
+        ))}
+      </ScrollView>
+    </SafeAreaView>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  scrollView: {
+    marginHorizontal: 20,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  content: {
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
 
 AppRegistry.registerComponent("App", () => Index);
